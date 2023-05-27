ea41e93e38646b07010f221b205f0c8e802db8b2
Yorqinovs <sunnatilloyorqinov2830@gmail.com>
2023-05-27 13:36:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// function first() {
+//   // code
+//   console.log(1);
+// }
+
+// function second() {
+//   // code
+//   console.log(2);
+// }
+
+// first();
+// second();
+
+////-------------------------------------------////
+
+// function edu(subject, callback) {
+//   console.log(`I wanna teach ${subject}`);
+//   callback();
+// }
+// edu("JavaScript", function () {
+//   console.log("That's great!");
+// });
+
+// function edu(subject, callback) {
+//   console.log(`I wanna teach ${subject}`);
+//   callback();
+// }
+// function done() {
+//   console.log("That's great!");
+// }
+// edu("JavaScript", done);
+
+////-------------------------------------------////
+
+// function first(cb) {
+//   // code
+//   setTimeout(() => {
+//     console.log(1);
+//   }, 1000);
+//   cb();
+// }
+// function second() {
+//   // code
+//   console.log(2);
+// }
+// first(second);
+//// Javobi 2 1 chiqazib beradi
